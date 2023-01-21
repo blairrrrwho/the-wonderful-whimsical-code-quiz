7512be0259b20e36ffa9f556d64d5bf0bb7a8587
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-01-21 03:27:34 -0600

    i have no idea what i'm doing

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,19 @@
 // const myBtn2 = document.createElement("button");
 // myBtn2.innerHTML = "this is another fucking button";
 // document.getElementById("part2").appendChild(myBtn2);
+
+let startBtn = document.getElementById("start-button");
+let homePage = document.getElementById("homepage");
+
+// Defines the startQuiz funtion 
+function startQuiz(){
+    homePage.classList.add("hidden");
+
+}
+
+// Calls the startQuiz function when the start button is clicked
+startBtn.addEventListener('click', startQuiz());
+
+startQuiz();
+
+//  ^^ does this work? 
