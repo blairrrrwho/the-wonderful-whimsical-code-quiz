6b50932b41fda4ae7ceecd08726e7716e84ad7c3
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-01-21 00:39:44 -0600

    completely redid html layout

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+const part1 = document.getElementById("part1");
+const part2 = document.getElementById("part2");
+const myBtn1 = document.getElementById("btn");
+
+// functionStart = function () {}
+// myBtn.addEventListener("click", nameoffunction); 
+
+
+
+
+part1.querySelector("h1").innerHTML = "this fucking sucks";
+
+
+part2.querySelector("button").innerHTML = "this is an answer";
+
+const myBtn2 = document.createElement("button");
+myBtn2.innerHTML = "this is another fucking button";
+document.getElementById("part2").appendChild(myBtn2);
