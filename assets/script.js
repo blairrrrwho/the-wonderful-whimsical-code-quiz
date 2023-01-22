4b3b586b1327a21aacda9b32f307195b266d304b
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-01-21 21:33:44 -0600

    got the startGame function to workgit add -A; haven't inputted data for quest/answers yet, but got the first screen to change to the first question when start button is clicked

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,19 @@
+const startButton = document.getElementById("start-button");
+const homePage = document.getElementById("homepage");
+const questionContainerElement = document.getElementById("question-container");
 
+startButton.addEventListener("click", startGame);
 
 // What needs to happen when you click the start button 
 function startGame() {
+    // Test to make sure our startGame function is being called w/startBtn when clicked
+    console.log("started");
+    // Need to hide the startBtn + title page; need to display the first set of questions
+    startButton.classList.add("hide");
+    homePage.classList.add("hide");
+    questionContainerElement.classList.remove("hide");
+
+
 
 }
 
