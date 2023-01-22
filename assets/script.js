ee266d328577079ea4176e80aabfc9437555520e
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-01-21 21:12:21 -0600

    wrote out the main functions we are going to need to set up the work flow

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,17 @@
-// const part1 = document.getElementById("part1");
-// const part2 = document.getElementById("part2");
-// const myBtn1 = document.getElementById("btn");
-
-// functionStart = function () {}
-// myBtn.addEventListener("click", nameoffunction); 
 
 
-
-
-// part1.querySelector("h1").innerHTML = "this fucking sucks";
-
-
-// part2.querySelector("button").innerHTML = "this is an answer";
-
-// const myBtn2 = document.createElement("button");
-// myBtn2.innerHTML = "this is another fucking button";
-// document.getElementById("part2").appendChild(myBtn2);
-
-let startBtn = document.getElementById("start-button");
-let homePage = document.getElementById("homepage");
-
-// Defines the startQuiz funtion 
-function startQuiz(){
-    homePage.classList.add("hidden");
+// What needs to happen when you click the start button 
+function startGame() {
 
 }
 
-// Calls the startQuiz function when the start button is clicked
-startBtn.addEventListener('click', startQuiz());
+// Function that will set up the next question
+// Next button or set up so that choice clicked from previous question acts as next button
+function setNextQuestion() {
 
-startQuiz();
+}
 
-//  ^^ does this work? 
+// Does something when the user selects an answer
+function selectAnswer() {
+
+}
